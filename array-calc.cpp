adb297ef3fb18bd138c1109f74959e7f5f9effbf
TeacherLi07 <110473389+TeacherLi07@users.noreply.github.com>
2023-01-18 22:18:05 +0800

    Add files via upload

--- array-calc.cpp
+++ array-calc.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 const int N=10005;
 
+void getinit();
 void gene_arr();
 void sum_arr();
-void rec_arr();
 void printans();
 void array2sum();
 void sum2array();
@@ -16,25 +16,25 @@ int evaluateExpression(string expression);
 
 string equa;
 int mode,a[N],s[N],n;
-string modewd[3]={"通项公式an=","前n项和公式sn=","递推公式an="};
+bool iscalca[N],iscalcs[N];
+string modewd[3]={"an=","sn="};
 int main()
 {
     system("cls");
     cout<<"*****************************************************************"<<endl;
     cout<<"                           数列计算器"<<endl;
     cout<<"                       Version alhpa1.1"<<endl;
-    cout<<"          编写者：TeacherLi李信辉,部分代码来源于ChatGPT"<<endl;
+    cout<<"          编写者：TeacherLi李信辉，部分代码来源于ChatGPT"<<endl;
     cout<<"*****************************************************************"<<endl<<endl;
-    Sleep(1000);
 L1:    cout<<"使用说明："<<endl<<endl;
-    cout<<"代数式用半角（英文）字符表示,支持加（+）减（-）乘（*)除(/)乘方(^),暂不支持根号。"<<endl<<endl;
+    cout<<"代数式用半角（英文）字符表示,支持加（+）减（-）乘（*）除（/）乘方（^）,暂不支持根号。"<<endl<<endl;
     cout<<"数列某项以an表示，前n项和以sn表示，计算总项数最大为100000项。"<<endl<<endl;
     cout<<"序数下标如果有多项，请为下标打括号。"<<endl<<endl;
     cout<<"合法表达例如：an；a(n+1)；a((3n+5)^2)；a114。"<<endl<<endl;
-    cout<<"乘方运算中，指数和底数如非单字母或纯数字，需要打括号"<<endl<<endl;
+    cout<<"乘方运算中，指数和底数如非单字母或纯数字，需要打括号。"<<endl<<endl;
     cout<<"表达式中n与x等价，大小写不敏感，不支持小数，可以用分数表示。"<<endl<<endl;
-    cout<<"如果输入非法或者无法计算，可能会有各种奇奇怪怪的现象，如果不确定是否为bug可以联系我（"<<endl<<endl<<endl;
-    cout<<"计算模式：\n1.由通项公式输出数列\n2.由前n项和输出数列\n3.由递推公式输出数列\n请输入模式：";
+    cout<<"如果输入非法或者无法计算，可能会有各种奇奇怪怪的现象，如果不确定是否为bug可以联系我或提交issue。"<<endl<<endl<<endl;
+    cout<<"计算模式：\n1.an表达式\n2.sn表达式\n表达式均可含有n、s、a\n请输入模式：";
     cin>>mode;
     cout<<"请输入"<<modewd[mode-1];
     cin>>equa;
@@ -56,14 +56,15 @@ L1:    cout<<"使用说明："<<endl<<endl;
             else if(whichsymbol=='/')
                 equa.insert(i,"/");
         }//处理多项式计算时，n、an、sn前系数无乘号问题，未处理乘方运算的优先级问题，所以在使用方法中已注明
+    cout<<"是否有已知数据？如有请输入如“s1=1”，“a3=2”，一项一行，如无或输入完成请输入-1"<<endl;
+    getchar();
+    getinit();
     cout<<"请输入计算项数 ";
     cin>>n;
     if(mode==1)
         gene_arr();
-    // else if(mode==2)
-    //     sum_arr();
-    // else if(mode==3)
-    //     rec_arr();
+    else if(mode==2)
+        sum_arr();
     else{
         cout<<"未知模式，请重新输入"<<endl<<endl;
         goto L1;
@@ -71,10 +72,48 @@ L1:    cout<<"使用说明："<<endl<<endl;
     printans();
 }
 
+void getinit()
+{
+    string init;
+    while(1)
+    {
+        getline(cin,init);
+        if(init=="-1")
+            return;
+        transform(init.begin(),init.end(),init.begin(),::tolower);
+        int val1 = 0,val2=0;
+        int i=1;
+        while (i < init.length() && isdigit(init[i]))
+        {
+            val1 = (val1*10) + (init[i] - '0');
+            i++;
+        }
+        i++;
+        while (i < init.length() && isdigit(init[i]))
+        {
+            val2 = (val2*10) + (init[i] - '0');
+            i++;
+        }
+        if(init[0]=='a')
+        {
+            a[val1]=val2;
+            iscalca[i]=1;
+        }
+        else if(init[0]=='s')
+        {
+           s[val1]=val2;
+            iscalcs[i]=1;
+        }
+    }
+}
+
 void gene_arr()
 {
+    sum2array();
     for(int i=1;i<=n;i++)
     {
+        if(iscalca[i])
+            continue;
         string equan=equa;
         for(int j=0;j<equan.length();j++)
         {
@@ -91,18 +130,37 @@ void gene_arr()
 
 void sum_arr()
 {
+    array2sum();
+    for(int i=1;i<=n;i++)
+    {
+        if(iscalca[i])
+            continue;
+        string equan=equa;
+        for(int j=0;j<equan.length();j++)
+        {
+            if(equan[j]=='n')
+            {
+                equan.erase(j,1);
+                equan.insert(j,to_string(i));
+            }
+        }
+        s[i]=evaluateExpression(equan);
+    }
+    sum2array();
 }
 
 void array2sum()
 {
     for(int i=1;i<=n;i++)
-        s[i]=a[i]+s[i-1];
+        if(!(iscalcs[i] || iscalca[i]))
+            s[i]=a[i]+s[i-1];
 }
 
 void sum2array()
 {
     for(int i=1;i<=n;i++)
-        a[i]=s[i]-s[i-1];
+        if(!(iscalcs[i] || iscalca[i]))
+            a[i]=s[i]-s[i-1];
 }
 
 void printans()
@@ -110,6 +168,7 @@ void printans()
     cout<<"n\tan\tSn\t"<<endl;
     for(int i=1;i<=n;i++)
         cout<<i<<"\t"<<a[i]<<"\t"<<s[i]<<"\t"<<endl;
+    cout<<"计算完毕，";
     system("pause");
 }
 
@@ -174,7 +233,7 @@ int evaluateExpression(string expression) {
                 {
                     flag++;
                 }
-                values.push(a[evaluateExpression(expression.substr(i+1,flag+1))]);
+                values.push(a[evaluateExpression(expression.substr(i+1,flag))]);
                 i+=flag;
             }
         }
